46a321ea41e826441dfc34c462a252d8de2245ec
Eben <eben.lowe@gmail.com>
2014-04-16 17:55:04 -0400

    Adding 'under construction' to index.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create, :show, :edit, :update]
 
+  get 'sign_in', to: 'pages#signin'
+
   get '/auth/:provider/callback', to: 'users#new'
 
 end
